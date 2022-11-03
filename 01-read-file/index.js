4a3cf74b4a4c375d1cfbc72664925c8e8974d867
Ivan Varabyou <onebelbiz@gmail.com>
2022-11-03 14:39:17 +0300

    feat: add 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,10 @@
-// start task HTML builder
+const fs = require('fs');
+const path = require('path');
+const { stdout } = require('process');
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8')
+
+let data = '';
+
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data))
+stream.on('error', error => console.error(error.message))
